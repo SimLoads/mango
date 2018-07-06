376596ce950f763afbda5e7e2217e843a99605dd
pygoose <35179439+SimLoads@users.noreply.github.com>
2018-07-06 19:00:48 +0100

    Update mango-core.py

--- mango-core.py
+++ mango-core.py
@@ -1,12 +1,5 @@
-###MANGO-CORE-0.0.0.1###
+###MANGO-CORE-0.0.0.0-ALPHA1TEST###
 '''
-Created on Fri Jul  6 14:25:30 2018
-Written in Python 3.7
 '''
-def core(pause):
-    print("Mango Core Call Success!")
-    import os
-    print("CD - " + os.getcwd())
-    if pause == True:
-        os.system("pause")
-
+def core_test(pause):
+    print("Mango Core Response Successful.")
